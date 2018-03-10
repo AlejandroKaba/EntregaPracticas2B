2e0fff51fa5bdba99979abeb99798053cfe57752
caballeroalejandro2009@hotmail.com <HP@LAPTOP-URMVOQ1J>
2018-03-10 16:52:21 +0100

    Version 1.0

---
+++ src/junit_shape/Shape.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package junit_shape;
+
+/**
+ *
+ * @author HP
+ */
+public class Shape {
+    private final double size;
+    public Shape(double size){
+        this.size=size;
+    }
+    public double getSize() {return size; }
+}
